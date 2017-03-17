de85a20999774a27e18341ac0179983a7ec7f5af
Chantz Large <chantz@connectionslab.com>
2017-03-17 17:19:10 -0600

    wip;

--- index.js
+++ index.js
@@ -1,12 +1,85 @@
 'use strict'
 
+const _ = require('lodash')
+
 /**
  * @class
- * @author Chantz Large <chantz@frontrange.co> (https://frontrange.co)
+ * @author Chantz Large <@chantzlarge>
  * @description TODO
 **/
 class Diff {
-  // TODO
+  /**
+   * @constructor
+   * @author Chantz Large <@chantzlarge>
+   * @description TODO
+   * @arg {Object|Array} expected - TODO
+   * @arg {Object|Array} actual - TODO
+   * @arg {Object} options - TODO
+  **/
+  constructor (expected, actual, options) {
+    this.expected = expected
+    this.actual = actual
+    this.options = options
+    this.pairs = { expected: [], actual: [] }
+    Diff.toPairsDeep(this.expected, this.pairs.expected)
+    Diff.toPairsDeep(this.actual, this.pairs.actual)
+  }
+
+  /**
+   * @setter
+   * @arg {Object|Array} expected - TODO
+  **/
+  set expected (expected) {
+    if (_.isObject(expected)) this._expected = expected
+    else throw new Error('Invalid argument: expected')
+  }
+
+  /**
+   * @setter
+   * @arg {Object|Array} expected - TODO
+  **/
+  set actual (actual) {
+    if (_.isObject(actual)) this._actual = actual
+    else throw new Error('Invalid argument: actual')
+  }
+
+  /**
+   * @getter
+   * @returns {Object|Array}
+  **/
+  get expected () {
+    return this._expected
+  }
+
+  /**
+   * @getter
+   * @returns {Object|Array}
+  **/
+  get actual () {
+    return this._actual
+  }
+
+  /**
+   * @function
+   * @author Chantz Large <@chantzlarge>
+   * @description TODO
+  **/
+  static toPairsDeep (o, target, path) {
+    if (_.isArray(o)) _.each(o, (v, i) => Diff.toPairsDeep(o[i], target, `${path}[${i}]`))
+    else if (_.isObject(o)) _.each(o, (v, k) => Diff.toPairsDeep(o[k], target, `${(path) ? `${path}.` : ''}${k}`))
+    else target.push([ path, o ])
+  }
+
+  /**
+   * @function
+   * @author Chantz Large <@chantzlarge>
+   * @description TODO
+   * @arg {Any} a - TODO
+   * @arg {Any} b - TODO
+  **/
+  static difference (a, b) {
+    return _.differenceWith(a, b, _.isEqual)
+  }
 }
 
 /**
--- test/test.diff.js
+++ test/test.diff.js
@@ -2,7 +2,22 @@
 
 const assert = require('assert')
 const Diff = require('../')
+const a = require('./a.json')
+const b = require('./b.json')
 
 describe('Diff', () => {
-  // TODO
+  describe('new Diff()', () => {
+    it('should construct instance of Diff', () => {
+      let diff = new Diff(a, b)
+      assert.equal('Diff', diff.constructor.name)
+      console.log(Diff.difference(diff.pairs.actual, diff.pairs.expected))
+    })
+  })
+
+  describe('toPairsDeep()', () => {
+    it('', () => {
+      let target = []
+      Diff.toPairsDeep(a, target)
+    })
+  })
 })
