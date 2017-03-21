38026ee57c52d1b0c1cd0a2151537aef0ecaf4c1
Chantz Large <chantz@connectionslab.com>
2017-03-20 21:51:04 -0600

    wip

--- index.js
+++ index.js
@@ -11,168 +11,85 @@ class Diff {
   /**
    * @constructor
   **/
-  constructor (expected, actual) {
-    this.expected = expected
-    this.actual = actual
-  }
-
-  /**
-   * @member
-  **/
-  get difference () {
-    return {
-      expected: Diff.difference(this.comparison.expected),
-      actual: Diff.difference(this.comparison.actual)
-    }
+  constructor (e, a) {
+    this.e = e
+    this.a = a
   }
 
   /**
    * @static
    * @function
-  **/
-  static difference (a) {
-    let valid = []
-    let invalid = []
-    let missing = []
-    _.each(a, (x) => {
-      if (_.isEqual(x[0], x[2]) && _.isEqual(x[1], x[3])) valid.push(x)
-      else if (_.isEqual(x[0], x[2])) invalid.push(x)
-      else missing.push(x)
-    })
-    return {
-      valid: valid,
-      invalid: invalid,
-      missing: missing
-    }
-  }
-
-  /**
-   * @member
-  **/
-  get comparison () {
-    return {
-      expected: Diff.compare(this.entries.expected, this.entries.actual),
-      actual: Diff.compare(this.entries.actual, this.entries.actual)
-    }
-  }
-
-  /**
-   * @static
-   * @function
-  **/
-  static compare (a, b) {
-    return _.map(a, (x, i) => {
-      let j = _.findIndex(b, (y) => {
-        return _.isEqual(x[0], y[0])
-      })
-      if (j === -1) {
-        x.push(null)
-        x.push(null)
-        return x
-      }
-      else {
-        x.push(b[j][0])
-        x.push(b[j][1])
-        return x
-      }
-    })
-  }
-
-  /**
-   * @member
-  **/
-  get entries () {
-    return {
-      expected: Diff.entries(this.expected),
-      actual: Diff.entries(this.actual)
-    }
-  }
-
-  /**
-   * @static
-   * @function
-  **/
-  static entries (object) {
-    let entries = []
-    Diff.toEntries(object, entries)
-    return entries
-  }
-
-  /**
-   * @static
-   * @function
-  **/
-  static toEntries (object, target, path) {
-    if (_.isObject(object)) _.each(object, (value, key) => Diff.toEntries(value, target, path ? path.concat(key) : [].concat(key)))
-    else target.push([ path, object ])
-  }
-
-  /**
-   * @member
-  **/
-  get pairs () {
-    return {
-      expected: Diff.toPairs(this.entries.expected),
-      actual: Diff.toPairs(this.entries.actual)
-    }
-  }
-
-  /**
-   * @static
-   * @function
-  **/
-  static toPairs (entries) {
-    return _.map(entries, (entry) => {
-      return [Diff.toPath(entry[0]), entry[1]]
-    })
-  }
-
-  /**
-   * @static
-   * @function
-  **/
-  static toPath (keys) {
-    let path = ''
-    _.each(keys, (key) => {
-      path += _.isNumber(key) ? `[${key}]` : `${path ? '.' : ''}${key}`
-    })
-    return path
-  }
-
-  /**
-   * @member
-  **/
-  get print () {
-    return {
-      difference: {
-        expected: Diff.print.difference.expected(this.difference.expected)
-      }
-    }
-  }
-
-  /**
-   * @static
-   * @member
   **/
   static get print () {
     return {
-      difference: {
-        expected: (diff) => {
-          console.log(`\nValid (${diff.valid.length} / ${diff.valid.length + diff.invalid.length + diff.missing.length}):\n`)
-          _.each(diff.valid, (entry) => {
-            console.log(`${Diff.toPath(entry[0])}: ${entry[1]} === ${entry[3]}`.green)
-          })
-          console.log(`\nInvalid (${diff.invalid.length} / ${diff.valid.length + diff.invalid.length + diff.missing.length}):\n`)
-          _.each(diff.invalid, (entry) => {
-            console.log(`${Diff.toPath(entry[0])}: ${entry[1]} !== ${entry[3]}`.yellow)
-          })
-          console.log(`\nMissing (${diff.missing.length} / ${diff.missing.length + diff.invalid.length + diff.missing.length}):\n`)
-          _.each(diff.missing, (entry) => {
-            console.log(`${Diff.toPath(entry[0])}: ${entry[1]} !== ${entry[3]}`.red)
-          })
-        }
-      }
+      asPathes: (a, b) => console.log(Diff.asPathes(a, b))
     }
+  }
+
+  /**
+   * @static
+   * @function
+  **/
+  static asPathes (a, b) {
+    let s = ''
+    _.each(Diff.difference(a, b), (v, k) => {
+      let t = `${Diff.toPath(v[0])}: ${v[1]} → ${v[2]}\n`
+      if (_.isEqual(v[1], v[2])) s += t.green
+      else if (v[1] && v[2]) s += t.yellow
+      else s += t.red
+    })
+    return s
+  }
+
+  /**
+   * @static
+   * @function
+  **/
+  static difference (a, b) {
+    a = Diff.entries(a)
+    b = Diff.entries(b)
+    return _.map(a, (x, i) => {
+      let j = _.findIndex(b, (y) => { return _.isEqual(x[0], y[0]) })
+      if (j === -1) return x.concat(undefined)
+      else return x.concat(b[j][1])
+    })
+  }
+
+  /**
+   * @static
+   * @function
+  **/
+  static entries (o) {
+    let e = []
+    Diff.toEntries(o, e)
+    return e
+  }
+
+  /**
+   * @static
+   * @function
+  **/
+  static toEntries (o, t, p) {
+    _.isObject(o) ? _.each(o, (v, k) => Diff.toEntries(v, t, p ? p.concat(k) : [].concat(k))) : t.push([p, o])
+  }
+
+  /**
+   * @static
+   * @function
+  **/
+  static toPairs (o) {
+    let e = Diff.entries(o)
+    return _.map(e, (v, k) => { return [Diff.toPath(v[0]), v[1]] })
+  }
+
+  /**
+   * @static
+   * @function
+  **/
+  static toPath (p) {
+    let str = ''
+    _.each(p, (v) => str += _.isNumber(p) ? `[${v}]` : `${str ? '.' : ''}${v}`)
+    return str
   }
 }
 
--- test/test.diff.js
+++ test/test.diff.js
@@ -6,76 +6,7 @@ const a = require('./a.json')
 const b = require('./b.json')
 
 describe('Diff', () => {
-  describe('new Diff()', () => {
-    it('', () => {
-      let d = new Diff(a, b)
-    })
-  })
-
-  describe('#entries()', () => {
-    it('', () => {
-      let e = Diff.entries(a)
-    })
-  })
-
-  describe('#entries', () => {
-    it('', () => {
-      let d = new Diff(a, b)
-      // console.log(d.entries)
-    })
-  })
-
-  describe('#compare()', () => {
-    it('', () => {
-      let d = new Diff(a, b)
-      let comparison = Diff.compare(d.entries.expected, d.entries.actual)
-    })
-  })
-
-  describe('#comparison', () => {
-    it('', () => {
-      let d = new Diff(a, b)
-      // console.log(d.comparison)
-    })
-  })
-
-  describe('#difference()', () => {
-    it('', () => {
-      let d = new Diff(a, b)
-      let diff = Diff.difference(d.comparison.expected)
-    })
-  })
-
-  describe('#difference', () => {
-    it('', () => {
-      let d = new Diff(a, b)
-      d.difference
-    })
-  })
-
-  describe('#toPath()', () => {
-    it('', () => {
-      let d = new Diff(a, b)
-      let path = Diff.toPath(d.entries.expected[0])
-    })
-  })
-
-  describe('#toPairs()', () => {
-    it('', () => {
-      let d = new Diff(a, b)
-      let path = Diff.toPairs(d.entries.expected)
-    })
-  })
-
-  describe('#pairs', () => {
-    it('', () => {
-      let d = new Diff(a, b)
-      d.pairs
-    })
-  })
-
-  describe('print.difference.expected()', () => {
-    let d = new Diff(a, b)
-    d.print.expected
+  it('', () => {
+    Diff.print.asPathes(a, b)
   })
 })
